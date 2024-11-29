8b55ed189f6f138a9074a5a4548bf2942bf5506e
itsachrafmansari <contact@achrafmansari.com>
2024-11-29 10:40:48 +0100

    Modified the JWT token API path

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/', include('users.urls')),
 ]
