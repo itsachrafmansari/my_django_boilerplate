3eb13dd82a707b5f37fdba40f860a78473ed373d
itsachrafmansari <contact@achrafmansari.com>
2024-12-01 15:49:33 +0100

    Added base test classes for Signup, Login and Logout

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import CustomUser
+
+
+class Tests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.active_user_data = {'email': 'active@example.com', 'password': 'active123'}
+        self.inactive_user_data = {'email': 'inactive@example.com', 'password': 'inactive123'}
+        self.nonexistent_user_data = {'email': 'nonexistent@example.com', 'password': 'nonexistent123'}
+
+        self.active_user = CustomUser.objects.create_user(**self.active_user_data, is_active=True)
+        self.inactive_user = CustomUser.objects.create_user(**self.inactive_user_data)
+
+
+class SignupTests(Tests):
+
+    def setUp(self):
+        super().setUp()
+
+        self.signup_url = reverse('signup-view')
+        self.email_verification_url = lambda uidb64, token: reverse(
+            'email-verification-view', args=(uidb64, token)
+        )
+
+
+class LoginTests(Tests):
+
+    def setUp(self):
+        super().setUp()
+
+        self.login_url = reverse('login-view')
+        self.password_reset_request_url = reverse('password-reset-request-view')
+        self.password_verification = lambda uidb64, token: reverse(
+            'password-reset-confirm-view', args=(uidb64, token)
+        )
+
+
+class LogoutTests(Tests):
+
+    def setUp(self):
+        super().setUp()
+        self.logout_url = reverse('logout-view')
