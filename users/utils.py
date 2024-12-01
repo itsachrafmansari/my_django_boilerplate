b63cb14c195956921ccb8344c6cfe77597562a45
itsachrafmansari <contact@achrafmansari.com>
2024-12-01 15:39:53 +0100

    Removed the unused utils.py file

--- users/utils.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-
-class EmailVerificationTokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
-        # Combine user data and timestamp to create a unique hash value
-        return f"{user.pk}{timestamp}{user.is_active}"
-
-email_verification_token = EmailVerificationTokenGenerator()
