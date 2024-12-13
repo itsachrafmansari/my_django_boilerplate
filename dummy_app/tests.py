7c936828797de016f3aaa6b64bc1a0e3e997f6ba
itsachrafmansari <contact@achrafmansari.com>
2024-12-13 13:13:41 +0100

    Added a test for creating dummies through unprotected API view

--- dummy_app/tests.py
+++ dummy_app/tests.py
@@ -1,3 +1,5 @@
+import json
+
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -27,3 +29,18 @@ class DummyTest(TestCase):
     def test_get_single_dummy_not_found(self):
         response = self.client.get(self.dummy_url(1000000))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_create_dummy(self):
+        data = {'label': 'Dummy Label', 'description': 'Some text', 'category': self.category.id}
+        response = self.client.post(self.dummy_url(), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['label'], data['label'])
+
+        data = [
+            {'label': 'Dummy Label 1', 'description': 'Some text', 'category': self.category.id},
+            {'label': 'Dummy Label 2', 'description': 'Some text', 'category': self.category.id},
+            {'label': 'Dummy Label 3', 'description': 'Some text', 'category': self.category.id},
+        ]
+        response = self.client.post(self.dummy_url(), data=json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data), 3)
