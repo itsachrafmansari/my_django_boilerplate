fa1a7b21fbead809d7fb6b2e77c87408e097155d
itsachrafmansari <contact@achrafmansari.com>
2024-12-13 13:02:36 +0100

    Fixed a typo in the dummy_app urls

--- dummy_app/urls.py
+++ dummy_app/urls.py
@@ -4,7 +4,7 @@ from .views import DummyView, DummyViewProtected
 
 urlpatterns = [
     path('dummy/', DummyView.as_view(), name='dummy-objects-view'),
-    path('dummy/<int:pk>', DummyView.as_view(), name='dummy-object-view'),
-    path('dummy/<int:pk>/protected', DummyViewProtected.as_view(), name='dummy-object-protected-view'),
-    path('dummy/<int:pk>/protected', DummyViewProtected.as_view(), name='dummy-object-protected-view'),
+    path('dummy/<int:pk>/', DummyView.as_view(), name='dummy-object-view'),
+    path('dummy/protected/', DummyViewProtected.as_view(), name='dummy-objects-protected-view'),
+    path('dummy/<int:pk>/protected/', DummyViewProtected.as_view(), name='dummy-object-protected-view'),
 ]
