9c7c9bdd408ee12fbe4841a1419b63ce9830d110
itsachrafmansari <contact@achrafmansari.com>
2024-12-13 13:04:53 +0100

    Added a test class for unprotected dummy views

--- dummy_app/tests.py
+++ dummy_app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import DummyCategory, Dummy
+
+
+class DummyTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.dummy_url = lambda pk=None: reverse('dummy-object-view', args=(pk,)) if pk else reverse('dummy-objects-view')
+
+        self.category = DummyCategory.objects.create(label="Category 1")
+        self.dummy = Dummy.objects.create(label="Dummy 1", description="Description 1", category=self.category)
